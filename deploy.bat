c6490809064435ac1dd5ec8dc422fb196100204e
Alexis <alexishirsch@vt.edu>
2025-10-31 22:54:00 -0400

    Initial commit: 100% workout tracking app

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "%~dp0"
+git init
+git add .
+git commit -m "Initial commit: 100%% workout tracking app"
+git remote add origin https://github.com/ahirsch17/100-.git 2>nul || git remote set-url origin https://github.com/ahirsch17/100-.git
+git branch -M main
+git push -u origin main
+pause
